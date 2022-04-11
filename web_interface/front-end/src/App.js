d4795525566831b7fc4e93dcb69520a61a5a5751
Polera78 <76696897+Polera78@users.noreply.github.com>
2022-04-11 16:56:15 +0200

    init front + login

---
+++ web_interface/front-end/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import './style/bootstrap.css';
+import './style/responsive.css';
+import './style/style.css';
+import login from './components/login.js';
+// import dashboard from './components/dashboard.js';
+
+{/*<dashboard/>*/}
+function App() {
+  return (
+	<login/> 
+	
+  );
+}
+
+export default login;
