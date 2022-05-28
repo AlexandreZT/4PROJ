e19f456afa923ae420addd65d92858a48e137efa
alexandreznoota <azt72629@gmail.com>
2022-05-29 00:10:35 +0200

    Forms completed

--- web_interface/frontend/src/components/member.js
+++ web_interface/frontend/src/components/member.js
@@ -1,11 +1,284 @@
 import React, { useState, useEffect } from 'react';
-// TODO : if user_type is staff, he can add users
+
 
 export default function Member() {
-    // create which kind of user : student, staff, tutor or teacher ?
-    // let chosen_user = "student" // default value
-    // let displayed_form = null
-    const  [state, setState] = useState({displayed_form: null, chosen_user: null});
+    // TODO : improve by giving a list of input & options
+    const display_student_form = () => {
+        return (
+            <div className="auth-inner">
+                <form onSubmit>
+                    <h3>Create Student</h3>
+                    <div className="form-group">
+                        <label>First name*</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter First Name" />
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Last name*</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Last Name" />
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Email</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Email" />
+                    </div>
+                    <br/>                           
+                    <div className="form-group">
+                        <label>Campus</label><br/>
+                        <select name="type">
+                        <option value="distanciel">Distanciel</option>
+                        <option value="paris">Paris</option>
+                        <option value="caen">Caen</option>
+                        <option value="lille">Lille</option>
+                        <option value="lyon">Lyon</option>
+                        <option value="tours">Tours</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label for="start">Birth date</label><br/>
+                        <input type="date" id="start" name="trip-start" min="1920-01-01"/>
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Address</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Address" />
+                    </div>
+                    <br/>                
+                    <div className="form-group">
+                        <label>Gender</label><br/>
+                        <select name="type">
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Region</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Region" />
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Level</label><br/>
+                        <select name="level">
+                            <option value="beng1">B.Eng.1</option>
+                            <option value="beng2">B.Eng.2</option>
+                            <option value="beng3">B.Eng.3</option>
+                            <option value="meng1">M.Eng.1</option>
+                            <option value="meng2">M.Eng.2</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Speciality</label><br/>
+                        <select name="spe">
+                            <option value="beng">B.Eng</option>
+                            <option value="ia">Intelligence Artificielle</option>
+                            <option value="srs">Systemes Reseaux et securité</option>
+                            <option value="data">Data Engineering</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Alternance</label><br/>
+                        <select name="cp">
+                            <option value="no">No</option>
+                            <option value="yes">Yes</option>                            
+                        </select>
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Previous level</label><br/>
+                        <select name="previous-level">
+                            <option value="bac">BAC</option>
+                            <option value="beng1">BTS</option>
+                            <option value="beng2">DUT</option>
+                            <option value="beng3">Licence</option>
+                            <option value="meng1">Master</option>
+                            <option value="beng1">B.Eng.1</option>
+                            <option value="beng2">B.Eng.2</option>
+                            <option value="beng3">B.Eng.3</option>
+                            <option value="meng1">M.Eng.1</option>
+                            <option value="meng2">M.Eng.2</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                    <label>Age of entry</label><br/>
+                        <input type="number" min="10" max="100"></input>
+                    </div>
+                    <br/>
+                    <button type="submit" className="btn btn-primary btn-block">Create</button>
+                    {/* {   
+                        this.state.response===null?
+                        <p></p>:
+                        this.state.response===200?
+                        <p style={{color:"green"}}>User created !</p>:
+                        this.state.response===500 &&
+                        <p style={{color:"red"}}>Something went wrong !</p>
+                    } */}
+                </form>
+            </div>
+        )       
+        // age_of_entry=None,
+
+    }
+    
+    const display_staff_form = () => {
+        return (
+            <div className="auth-inner">
+                <form onSubmit>
+                    <h3>Create Staff</h3>
+                    <div className="form-group">
+                        <label>First name*</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter First Name" />
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Last name*</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Last Name" />
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Email</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Email" />
+                    </div>
+                    <br/> 
+                    <div className="form-group">
+                        <label>Campus</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Campus" />
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Phone</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Phone" />
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Role</label><br/>
+                        <select name="type">
+                        <option value="coordonator">Coordinateur</option>
+                        <option value="full-professor">Full Professor</option>
+                        <option value="academic-director">Directeur academique</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <button type="submit" className="btn btn-primary btn-block">Create</button>
+                    {/* {   
+                        this.state.response===null?
+                        <p></p>:
+                        this.state.response===200?
+                        <p style={{color:"green"}}>User created !</p>:
+                        this.state.response===500 &&
+                        <p style={{color:"red"}}>Something went wrong !</p>
+                    } */}
+                </form>
+            </div>
+        )
+    }
+
+    const display_tutor_form = () => {
+        return (
+            <div className="auth-inner">
+                <form onSubmit>
+                    <h3>Create Tutor</h3>
+                    <div className="form-group">
+                        <label>First name*</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter First Name" />
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Last name*</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Last Name" />
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Email</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Email" />
+                    </div>
+                    <br/> 
+                    <div className="form-group">
+                        <label>Phone</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Phone" />
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Gender</label><br/>
+                        <select name="type">
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Enterprise Name</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Enterprise Name" />
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Enterprise Location</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Enterprise Location" />
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Job</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Job" />
+                    </div>
+                    <br/>
+                    <button type="submit" className="btn btn-primary btn-block">Create</button>
+                    {/* {   
+                        this.state.response===null?
+                        <p></p>:
+                        this.state.response===200?
+                        <p style={{color:"green"}}>User created !</p>:
+                        this.state.response===500 &&
+                        <p style={{color:"red"}}>Something went wrong !</p>
+                    } */}
+                </form>
+            </div>
+        )
+    }
+
+    const display_teacher_form = () => {
+        return (
+            <div className="auth-inner">
+                <form onSubmit>
+                    <h3>Create Teacher</h3>
+                    <div className="form-group">
+                        <label>First name*</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter First Name" />
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Last name*</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Last Name" />
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <label>Email*</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Email" />
+                    </div>
+                    <br/> 
+                    <div className="form-group">
+                        <label>Phone</label><br/>
+                        <input type="text" className="form-control" placeholder="Enter Phone" />
+                    </div>
+                    <br/>
+                    <button type="submit" className="btn btn-primary btn-block">Create</button>
+                    {/* {   
+                        this.state.response===null?
+                        <p></p>:
+                        this.state.response===200?
+                        <p style={{color:"green"}}>User created !</p>:
+                        this.state.response===500 &&
+                        <p style={{color:"red"}}>Something went wrong !</p>
+                    } */}
+                </form>
+            </div>
+        )
+    }
+
+    const  [state, setState] = useState({displayed_form: display_student_form(), chosen_user: "student"});
 
     function changeUserForm(e) {
         let {name, value} = e.target;
@@ -27,39 +300,11 @@ export default function Member() {
         }, [state]
     )
 
-    let display_student_form = () => {
-        return (
-            <div><p>ITS ME STUUDYY</p></div>
-        )
-    }
-    
-    let display_staff_form = () => {
-        return (
-            <div><p>ITS ME STAAFYY</p></div>
-        )
-    }
-
-    function display_tutor_form(){
-        return (
-            <div><p>ITS ME TUUTTYY</p></div>
-        )
-    }
-
-    function display_teacher_form(){
-        return (
-            <div><p>ITS ME TEAACHYY</p></div>
-        )
-    }
-
-    
-    // function select_displayed_form(){
-    //     console.log("chosen_user", chosen_user)
-    
-    
     return (
         <div>
+            <label>Select Form:&nbsp;</label>
+            
             <select onChange={changeUserForm}>
-                <option value="">User type</option>
                 <option value="student">student</option>
                 <option value="staff">staff</option>
                 <option value="tutor">tutor</option>
@@ -68,50 +313,4 @@ export default function Member() {
             {state.displayed_form}
         </div>
     )
-    
-    // return (
-    //     <div className="auth-inner">
-    //         <form onSubmit>
-    //             <h3>Create User</h3>
-    //             <div className="form-group">
-    //                 <label>First name</label><br/>
-    //                 <input type="text" className="form-control" placeholder="Enter Name" />
-    //             </div>
-    //             <br/>
-    //             <div className="form-group">
-    //                 <label>Last name</label><br/>
-    //                 <input type="text" className="form-control" placeholder="Enter Description" />
-    //             </div>
-    //             <br/>
-    //             <div className="form-group">
-    //                 <label>Price</label><br/>
-    //                 <input type="text" className="form-control" placeholder="Enter Price" />
-    //             </div>
-    //             <br/>                           
-    //             <div className="form-group">
-    //                 <label>Category</label><br/>
-    //                 <select name="type">
-    //                 <option value="">Category</option>
-    //                     <option value="side">Side</option>
-    //                     <option value="pizza">Pizza</option>
-    //                     <option value="burger">Burger</option>
-    //                     <option value="salad">Salad</option>
-    //                     <option value="cake">Cake</option>
-    //                     <option value="cold drink">Cold drink</option>
-    //                     <option value="hot drink">Hot drink</option>
-    //                 </select>
-    //             </div>
-    //             <br/>
-    //             <button type="submit" className="btn btn-primary btn-block">Create</button>
-    //             {   
-    //             this.state.response===null?
-    //             <p></p>:
-    //             this.state.response===200?
-    //             <p style={{color:"green"}}>Product created !</p>:
-    //             this.state.response===500 &&
-    //             <p style={{color:"red"}}>Something went wrong !</p>
-    //         }
-    //         </form>
-    //     </div>
-    // )
 }
