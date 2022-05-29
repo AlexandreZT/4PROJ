615c5a4df2e493fe2868abd576098907367434f6
alexandreznoota <azt72629@gmail.com>
2022-05-29 22:44:55 +0200

    putain

--- web_interface/frontend/src/components/directory.js
+++ web_interface/frontend/src/components/directory.js
@@ -2,38 +2,47 @@ import React, { useState, useEffect } from 'react';
 
 export default function Directory() {
 
-    const  [state, setState] = useState({user_filter: "users", data_displayed: null}); // users by default or by types
+    const  [state, setState] = useState({user_filter: "users", data_displayed: null, data: null}); // users by default or by types
 
-    function load_directory() {
-        var axios = require('axios');
+    function load_directory (callback) {
+        if (state.data == null) { // hack to stop requesting infiniti loop (function still called)
+            var axios = require('axios');
 
-        var config = {
-            method: 'get',
-            url: 'http://127.0.0.1:5000/users' //+ state.user_filter
-        };
+            var config = {
+                method: 'get',
+                url: "https://4projgroup24.pythonanywhere.com/users" // "http://127.0.0.1:5000/users // + state.user_filter // if uing filter
+            };
+            
+            axios(config)
+
+            .then(function (response) {
+                if (response) {
+                    callback(response.data); // Return your data inside the callback function
+                } else {
+                    callback(null); // Return null if data doesn't exists
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
+        }
         
-        console.log(config)
-        axios(config)
-        // .then(response => (
-        //     console.log(response.data)
-        // ))
-        .then(response => setState(
-            {...state, data_displayed: response}
-        ))
-        .catch(function (error) {
-            console.log(error);
-        }); 
     }
+    
+    
+    load_directory(function (mydata) { // Call load_directory to get your data
+        setState({...state, data: mydata});
+        // continue here
+    })
 
-    function displayUsers(){
-        if (state.data_displayed) {
-            return Object.keys(state.data_displayed).map( (id) => {
+    function displayUsers(data) {
+        if (data != null) {
+            return Object.keys(data).map( (id) => {
               return <tr>
                 {/* <td>{id}</td> */}
-                <td>{state.data_displayed[id]["firstname"]}</td>
-                <td>{state.data_displayed[id]["lastname"]}</td>
-                <td>{state.data_displayed[id]["email"]}</td>
-                <td>{state.data_displayed[id]["user_type"]}</td>
+                <td>{data[id]["firstname"]}</td>
+                <td>{data[id]["lastname"]}</td>
+                <td>{data[id]["email"]}</td>
+                <td>{data[id]["user_type"]}</td>
               </tr>;
             });
         } else {
@@ -42,16 +51,24 @@ export default function Directory() {
     }
 
     useEffect(() => {
-        load_directory()
-        console.log(state.data_displayed)
-    }, [state]);
-
+    },  [state]);
+    
     return (
         <div>
-            {state.user_filter==="users"
-
-            }
-            <p>Directory</p>
+            <table style={{width: "100%"}} border={2} cellPadding={4}>
+                <thead>
+                    <tr>
+                        {/* <td>id</td> */}
+                        <td>firstname</td>
+                        <td>lastname</td>
+                        <td>email</td>
+                        <td>user_type</td>
+                    </tr>            
+                </thead>
+                <tbody>
+                    {displayUsers(state.data)}
+                </tbody> 
+            </table>
         </div>
     )
 }
